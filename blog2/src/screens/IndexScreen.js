6a975143b256a7a4ab9d712473e8ec1b50b2ac27
drbarq <J.Tustin@gmail.com>
2019-09-09 15:40:10 -0600

    adding in reducer

--- blog2/src/screens/IndexScreen.js
+++ blog2/src/screens/IndexScreen.js
@@ -1,15 +1,19 @@
 import React, { useContext } from 'react'
-import {View, Text, StyleSheet, FlatList} from'react-native'
-import BlogContext from '../context/BlogContext'
+import {View, Text, StyleSheet, FlatList, Button} from'react-native'
+import { Context } from '../context/BlogContext'
 
 const IndexScreen = () => {
-    const blogPosts = useContext(BlogContext)
+    const { state, addBlogPost } =  useContext(Context)
     
     return (
         <View>
             <Text>Index Screen</Text>
+            <Button 
+                title="Add Post" 
+                onPress={addBlogPost}
+            />
             <FlatList
-                data={blogPosts}
+                data={state}
                 keyExtractor={(blogPost) => blogPost.title}
                 renderItem={({item}) => {
                     return <Text>{item.title}</Text>
